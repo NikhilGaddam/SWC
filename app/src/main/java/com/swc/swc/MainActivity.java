2d220bb129cc81c201d3fd030012e3950ba78884
Ashwin <ashwin.kulkarni128@gmail.com>
2018-11-03 14:48:52 +0530

    Test Commit

--- app/src/main/java/com/swc/swc/MainActivity.java
+++ app/src/main/java/com/swc/swc/MainActivity.java
@@ -1,9 +1,15 @@
 package com.swc.swc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button abc;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
